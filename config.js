ac282afc3717fdfb286a9a0b0ed37298f41ad27f
Jonah Rueb <ruebjonah@gmail.com>
2019-09-03 15:17:27 -0500

    Added initial files and configurations.

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    twilio: {
+        accountSid: process.env.TWILIO_ACCOUNT_SID,
+        authToken: process.env.TWILIO_AUTH_TOKEN,
+        phoneNumber: process.env.TWILIO_PHONE_NUMBER
+    }
+};
+
+module.exports = config;
