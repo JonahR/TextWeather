37c7a08d62c858d1ae7e524296be6e13250b3baa
Jonah Rueb <ruebjonah@gmail.com>
2019-09-04 16:41:47 -0500

    Added route for sms.

---
+++ routes/sms.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const twiml = new MessagingResponse();
+
+    twiml.message('The Robots are coming! Head for the hills!');
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const http = require('http');
 const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const sms = require('./routes/sms');
 
 const app = express();
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
-
-  twiml.message('The Robots are coming! Head for the hills!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-});
+app.use('/sms', sms);
 
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
