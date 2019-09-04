86553b43db34f96855070c7dcda95c360dffda6e
Jonah Rueb <ruebjonah@gmail.com>
2019-09-04 12:52:53 -0500

    Added express server and connect to openweather API

--- config.js
+++ config.js
@@ -3,6 +3,9 @@ const config = {
         accountSid: process.env.TWILIO_ACCOUNT_SID,
         authToken: process.env.TWILIO_AUTH_TOKEN,
         phoneNumber: process.env.TWILIO_PHONE_NUMBER
+    },
+    openWeather: {
+        apiKey: process.env.OPENWEATHER_API_KEY
     }
 };
 
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const config = require('./config');
+const axios = require('axios');
+const client = require('twilio')(config.twilio.accountSid, config.twilio.authToken);
 
-console.log('Hello World')
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+function getWeatherByCityName(city){
+        axios.get(`${apiURL}?q=${city}&APPID=${config.openWeather.apiKey}`).then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+function sendMessage(text) {
+    client.messages.create({
+        body: text,
+        from: config.twilio.phoneNumber,
+        to: '+16512950514'
+    })
+    .then(message => console.log(message.sid))
+}
+
+sendMessage('Hello');
+
+getWeatherByCityName('Edina');
+
+
+
